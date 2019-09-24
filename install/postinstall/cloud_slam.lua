bc1f2d67c2f1e59f2291712ac2d177128ebf31e7
Shreyas Gokhale <sgo@elio.fokus.fraunhofer.de>
2019-09-24 21:16:08 +0200

    Bugfixes

--- install/postinstall/cloud_slam.lua
+++ install/postinstall/cloud_slam.lua
@@ -11,15 +11,15 @@ options = {
   tracking_frame = "imu_link",
   published_frame = "base_link",
   odom_frame = "odom",
-  provide_odom_frame = true,
+  provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 0,
+  num_laser_scans = 2,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 1,
+  num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -39,7 +39,7 @@ TRAJECTORY_BUILDER_2D.min_range = 1 -- Min range of the LIDAR, default 0
 TRAJECTORY_BUILDER_2D.max_range = 40. -- Max range of the LIDAR, default 30
 --TRAJECTORY_BUILDER_2D.submaps.num_range_data  =  90-- Size of submaps Default 90
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 160 -- This kinda works likes sampling. Setting 0 = no sampling. Default 1
+--TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 160 -- This kinda works likes sampling. Setting 0 = no sampling. Default 1
 --TRAJECTORY_BUILDER_2D.laser_min_range = 0.01
 --TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = false
 
